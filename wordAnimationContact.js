fd61a9b9aab82e0605e6f749ca358975ff364467
Unknown <mjohnson030@west-mec.org>
2019-05-11 20:10:01 -0700

    Commit 12
    Fixed spacing of paragraphs and photos/videos on home and About page. Added title above navigation on the home page. Added Javascript animation to all titles and stylized the home page.

--- wordAnimationContact.js
+++ wordAnimationContact.js
@@ -1,13 +1,6 @@
-// var string = "~ INFORMATION ~";
-// var str = string.split("");
-// var el = document.getElementById('str');
-// (function animate() {
-// str.length > 0 ? el.innerHTML += str.shift( ) : clearTimeout(running); 
-// var running = setTimeout(animate, 90);
-// })();
 
 function wordAnimation() {
-    var string = "~ INFORMATION ~";
+    var string = "~ CONTANCT ~";
     var str = string.split("");
     var el = document.getElementById('str');
     (function animate() {
--- wordAnimationHome.js
+++ wordAnimationHome.js
@@ -1,13 +1,5 @@
-// var string = "~ INFORMATION ~";
-// var str = string.split("");
-// var el = document.getElementById('str');
-// (function animate() {
-// str.length > 0 ? el.innerHTML += str.shift( ) : clearTimeout(running); 
-// var running = setTimeout(animate, 90);
-// })();
-
 function wordAnimation() {
-    var string = "~ INFORMATION ~";
+    var string = "MEDICAL ASSISTING";
     var str = string.split("");
     var el = document.getElementById('str');
     (function animate() {
