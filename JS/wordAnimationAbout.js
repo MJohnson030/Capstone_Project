f9044ac418df7f0b002b954b23ace12c7404f32c
Unknown <mjohnson030@west-mec.org>
2019-05-13 18:09:08 -0700

    Commit 19
    Updated the title of each slide

--- JS/wordAnimationAbout.js
+++ JS/wordAnimationAbout.js
@@ -7,14 +7,14 @@
 // })();
 
 function wordAnimation() {
-    var string = "~ INFORMATION ~";
+    var string = "INFORMATION";
     var str = string.split("");
     var el = document.getElementById('str');
     (function animate() {
-    str.length > 0 ? el.innerHTML += str.shift( ) : clearTimeout(running); 
-    var running = setTimeout(animate, 90);
+        str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+        var running = setTimeout(animate, 90);
     })();
-     
+
 }
 
 setTimeout(wordAnimation, 1500);
